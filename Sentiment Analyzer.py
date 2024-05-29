da8450aca1e66c29857ac6ab6928adc9ca5de43c
MichaelMDCJR <mcarlson7404@gmail.com>
2024-05-29 13:13:57 -0500

    Title, text, and comments are being rated and put into data frame

--- Sentiment Analyzer.py
+++ Sentiment Analyzer.py
@@ -11,6 +11,7 @@ from io import BytesIO
 import pandas
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from IPython.display import display
+import datetime
 
 
 # posts = []
@@ -23,6 +24,14 @@ from IPython.display import display
 # testing = []
 
 async def main():
+    # TEMP CREATE DATA FRAME
+    # [titlepos, titleneg, titleneu, titlerating, textpos, textneg, textneu, textrating,commpos, comneg, comneu, comrating, date]
+    frame = pandas.DataFrame(columns=['titlepos', 'titleneg', 'titleneu', 'titlerating', 'textpos', 'textneg', 'textneu', 'textrating','commpos', 'comneg', 'comneu', 'comrating', 'date'])
+    #frame.columns = ["Title"]
+
+    pandas.set_option("display.max_columns", None)
+    display(frame)
+
     # The number of post actually being used, as we discard videos
     posts_grabbed = 0
 
@@ -35,17 +44,11 @@ async def main():
 
     # Gets the subreddit and loads in the hot submissions into a dictionary
     #subreddit = await reddit.Front()
-    subreddit = await reddit.subreddit("politics")
-    async for submission in subreddit.top(limit=50, time_filter="day"):
-        #print(str(submission.url))
-        # posts.append({
-        #     "title": submission.title,
-        #     "author": str(submission.author),
-        #     "image": submission.url,
-        #     "text": submission.selftext,
-        # })
+    subreddit = await reddit.subreddit("popular")
+    async for submission in subreddit.top(limit=5, time_filter="day"):
         # [titlepos, titleneg, titleneu, titlerating, textpos, textneg, textneu, commpos, comneg, comneu, comrating, date]
         title = submission.title
+        print(title)
         sentiment_dict = sent_analyzer.polarity_scores(title)
         title_neg_score = sentiment_dict['neg'] * 100
         title_pos_score = sentiment_dict['pos'] * 100
@@ -69,53 +72,111 @@ async def main():
                 text_overall_rating = "Negative"
             else:
                 text_overall_rating = "Neutral"
+        else:
+            text_pos_score = pandas.NA
+            text_neu_score = pandas.NA
+            text_neg_score = pandas.NA
+            text_overall_rating = pandas.NA
 
         #comment_neu_avg =
 
         num_com = 5
+        grabbed_com = 0
+        total_com_pos = 0
+        total_com_neg = 0
+        total_com_neu = 0
+        com_rating_pos = 0
+        com_rating_neg = 0
+        com_rating_neu = 0
+
+
 
         #https://asyncpraw.readthedocs.io/en/stable/tutorials/comments.html#extracting-comments
 
+        # Get the submission comments
         comments = await submission.comments()
-        await comments.replace_more(limit=0)
-        for top_level_comment in comments:
-            print(top_level_comment.author)
+        #await comments.replace_more(limit=0)
+
+        # For a number of comments, iterate through them, recording sentiment
+        i = 0
+        while (i < num_com):
+        #for i in num_com:
+            #TESTING REMOVE LATER
+            com_overall_rating = ""
+
+            top_level_comment = comments[i]
+            # Ignore comments by automoderator
+            if top_level_comment.author != "Automoderator":
+                print(top_level_comment.author)
+
+                com_text = top_level_comment.body
+                sentiment_dict = sent_analyzer.polarity_scores(com_text)
+                total_com_neg += sentiment_dict['neg'] * 100
+                total_com_pos += sentiment_dict['pos'] * 100
+                total_com_neu += sentiment_dict['neu'] * 100
+                if sentiment_dict['compound'] >= 0.05:
+                    com_rating_pos += 1
+                elif sentiment_dict['compound'] <= -0.05:
+                    com_rating_neg += 1
+                else:
+                    com_rating_neu += 1
+
+                # If comments are a majority polarity, set that as the overall rating
+                if com_rating_neu >= 3:
+                    com_overall_rating = "Neutral"
+                elif com_rating_neg >= 3:
+                    com_overall_rating = "Negative"
+                elif com_rating_pos >= 3:
+                    com_overall_rating = "Positive"
+
+                # If the ratings are mixed with neutral, prefer the pos or neg rating
+                if com_rating_neu == 2 and com_rating_pos == 2:
+                    com_overall_rating = "Positive"
+                elif com_rating_neu == 2 and com_rating_neg == 2:
+                    com_overall_rating = "Negative"
+                # If ratings are split pos and neg, choose based on polarity scores
+                elif com_rating_pos == 2 and com_rating_neg == 2:
+                    if total_com_pos > total_com_neg:
+                        com_overall_rating = "Positive"
+                    else:
+                        com_overall_rating = "Negative"
+
+            # If there is an automod comment, add 1 to the number of comments to be grabbed
+            else:
+                num_com += 1
+                print("YAYYYYYYYYYY")
+                print("Num of com" + str(num_com))
+
+            i += 1
+
 
         posts_grabbed += 1
 
-    # for i in range(posts_grabbed):
-    #     print(posts[i]["title"])
-    #     if posts[i]["text"] != None:
-    #        print(posts[i]["text"] + "\n\n")
+        print(total_com_neu)
+        print(total_com_neg)
+        print(total_com_pos)
+        total_com_pos = total_com_pos/5.0
+        total_com_neg = total_com_neg/5.0
+        total_com_neu = total_com_neu/5.0
 
-        '''
-        # converts the posts grabbed to raw data to then be put in an image object
-        with urllib.request.urlopen(posts[i]["image"]) as u:
-            raw_data = u.read()
 
-        image = Image.open(io.BytesIO(raw_data))
+        # [titlepos, titleneg, titleneu, titlerating, textpos, textneg, textneu, textrating, commpos, comneg, comneu, comrating, date]
+        # Loop adding rows to data frame
+        print(com_rating_pos)
+        print(com_rating_neg)
+        print(com_rating_neu)
 
-        # image is reduced to a smaller size but with same dimesions
-        image.thumbnail((550,550))
-        #photo = ImageTk.PhotoImage(image)
-        '''
-    frame = pandas.DataFrame(title)
-    frame.columns=["Title"]
-    # #if selftext ==:
-    #     #frame['Text'] = selftext
-    # frame['Positive'] = pos_score
-    # frame['Negative'] = neg_score
-    # frame['Neutral'] = neu_score
-    # frame['Overall'] = overall_rating
+        # Create date object
+        date = datetime.datetime.now()
+        #print(date.strftime("%x"))
 
-    pandas.set_option("display.max_columns", None)
-    display(frame)
-
-    #for i in range(posts_grabbed):
+        # Add row to data frame
+        frame.loc[len(frame.index)] = [title_pos_score, title_neg_score, title_neu_score, title_overall_rating, text_pos_score, text_neg_score, text_neu_score, text_overall_rating, total_com_pos, total_com_neg, total_com_neu, com_overall_rating, date.strftime("%x")]
 
 
     await reddit.close()
 
+    display(frame)
 
 
 def regular():
